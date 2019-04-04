b17625fe0622718e8378d24cfd235810eb02b163
FernandaBaez <1630386@upv.edu.mx>
2019-04-04 06:04:56 +0000

    first commit

---
+++ wp-config.php
@@ -0,0 +1,84 @@
+ 
+<?php
+/**
+ * Configuración básica de WordPress.
+ *
+ * Este archivo contiene las siguientes configuraciones: ajustes de MySQL, prefijo de tablas,
+ * claves secretas, idioma de WordPress y ABSPATH. Para obtener más información,
+ * visita la página del Codex{@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} . Los ajustes de MySQL te los proporcionará tu proveedor de alojamiento web.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
+/** El nombre de tu base de datos de WordPress */
+define( 'DB_NAME', 'wordp' );
+
+/** Tu nombre de usuario de MySQL */
+define( 'DB_USER', 'root' );
+
+/** Tu contraseña de MySQL */
+define( 'DB_PASSWORD', 'BAEZ706534' );
+
+/** Host de MySQL (es muy probable que no necesites cambiarlo) */
+define( 'DB_HOST', 'localhost' );
+
+/** Codificación de caracteres para la base de datos. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** Cotejamiento de la base de datos. No lo modifiques si tienes dudas. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Claves únicas de autentificación.
+ *
+ * Define cada clave secreta con una frase aleatoria distinta.
+ * Puedes generarlas usando el {@link https://api.wordpress.org/secret-key/1.1/salt/ servicio de claves secretas de WordPress}
+ * Puedes cambiar las claves en cualquier momento para invalidar todas las cookies existentes. Esto forzará a todos los usuarios a volver a hacer login.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY', 'Gh/kk;M~; PLB|H_cI,Xfj6o&nlX2+ri+<tcoT>0%kqaT16Ib[O#A&B*tMNQj2}Q' );
+define( 'SECURE_AUTH_KEY', '#J~J=.(Yg3ZJ5Phg_/ P.5ultX2l={l2bEJUzs6L(&s+L(G3on>46@=DHnmTV=LN' );
+define( 'LOGGED_IN_KEY', 'ZKR6$l|A*;&_b9E;H^l$CB]>lm]Y9eZg}h)Z6zYC*MF4+N|MGlYZWmUl@@]e~9]2' );
+define( 'NONCE_KEY', 'Fn(UP@tF;mA~,=KspJZ1~aC.VBc<7i^SJJz>!om<;@22;gR5MM71(3Sg;x9ScQ@|' );
+define( 'AUTH_SALT', 'RTKsl[3,IargNnOx1Ir:SHj#qB:++hu75[PaGZ{d`%:]J.M{>@mx6co1u|w9V=!P' );
+define( 'SECURE_AUTH_SALT', 'L{PV!cMs5}s@~l6G$|@#% aSL4m /5Sko6ylD^|L5E#VIK}QJ%QFNj)R$^K.K>>J' );
+define( 'LOGGED_IN_SALT', 'IDxVY$vlexE[hTiFT<8X}cDRxi*}q=Dj[#Y:hYmLPZ}jQ21,IJzV/!E/JjO<h(bI' );
+define( 'NONCE_SALT', 'Kx1idm * ?iqqHAl6@pxQ.XcLwcM4DBSC-49CoSCyoR<TnOew^QL q@Tq$D)e9t#' );
+
+/**#@-*/
+
+/**
+ * Prefijo de la base de datos de WordPress.
+ *
+ * Cambia el prefijo si deseas instalar multiples blogs en una sola base de datos.
+ * Emplea solo números, letras y guión bajo.
+ */
+$table_prefix = 'wp_';
+
+
+/**
+ * Para desarrolladores: modo debug de WordPress.
+ *
+ * Cambia esto a true para activar la muestra de avisos durante el desarrollo.
+ * Se recomienda encarecidamente a los desarrolladores de temas y plugins que usen WP_DEBUG
+ * en sus entornos de desarrollo.
+ */
+define('WP_DEBUG', false);
+
+/* ¡Eso es todo, deja de editar! Feliz blogging */
+
+/** WordPress absolute path to the Wordpress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+define('FS_METHOD', 'direct');
+
